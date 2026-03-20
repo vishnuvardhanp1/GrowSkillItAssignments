8c73ff37da1642223f55c4ef897a67222d0a03eb
vishnuvardhanp1 <vishnu.vis2007@gmail.com>
2026-03-20 13:33:39 +0530

    checkin intial changes

---
+++ src/assignments/Student.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package assignments;
+/**
+ * 
+ */
+public class Student {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    byte age=44;
+    short marks=450;
+    byte mobileBatteryPercentage=100;
+    int cityPopulation = 2500000;
+    char grade='A';
+    int bankBalance=5000000;
+    boolean userLoginStatus=true;
+    int distanceToCollegeInMeters = 1200;
+    long largeNumber = 9000000000L;
+    System.out.println("Age: " + age);
+    System.out.println("Marks: " + marks);
+    System.out.println("Battery: " + mobileBatteryPercentage + "%");
+    System.out.println("City Population: " + cityPopulation);
+    System.out.println("Grade: " + grade);
+    System.out.println("Bank Balance: " + bankBalance);
+    System.out.println("Login Status: " + userLoginStatus);
+    System.out.println("Distance: " + distanceToCollegeInMeters + " meters");
+    System.out.println("Large Number: " + largeNumber);
+	}
+
+}
