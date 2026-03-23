3690a60d3646e7f5b10842f2f41dee2cd25b176f
vishnuvardhanp1 <vishnu.vis2007@gmail.com>
2026-03-23 16:20:31 +0530

    23 mar 2026 assignment program

---
+++ src/assignments/Program_EscapeSequence.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package assignments;
+
+/**
+ * 
+ */
+public class Program_EscapeSequence {
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		System.out.println("Value of \"a\":"+ a +"\nAfter increment: \t " + (a + 2));
+
+	}
+
+}
