4e97d79a224f30f37183ee38b9c6064c94487ec8
vishnuvardhanp1 <vishnu.vis2007@gmail.com>
2026-03-25 21:08:47 +0530

    25 mar assignment programs

---
+++ src/assignments/Program_Multiplication.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package assignments;
+
+/**
+ * 
+ */
+public class Program_Multiplication {
+
+	/**
+	 * @param args
+	 */
+	
+	public static void getValues() {
+	    int number=1;
+	    while(number <= 50) {
+	    	if(number%2 == 0 && number%5 == 0) {
+	    		System.out.println(number*number);
+	    	}
+	    	else if(number%2 == 0 ) {
+	    		System.out.println(number*number*number);
+	    	}
+	    	else if(number%5 == 0) {
+	    		System.out.println(number*5);
+	    	}
+	    	else {
+	    		System.out.println(number);
+	    	}
+	    	number++;
+	    }
+			
+	}
+	
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		getValues();
+		
+	}
+
+}
