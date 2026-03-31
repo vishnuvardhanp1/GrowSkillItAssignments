38cbcabdd90536b88767500d313db2d4380f1b28
vishnuvardhanp1 <vishnu.vis2007@gmail.com>
2026-03-31 17:21:02 +0530

    March 31 DailyAssignment patterns

---
+++ src/assignments/DailyAssignment9_Pattern1.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package assignments;
+
+/**
+ * 
+ */
+public class DailyAssignment9_Pattern1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=5;i>=1;i--) {
+			for(int j=1;j<=i;j++) {
+			System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+
+}
---
+++ src/assignments/DailyAssignment9_Pattern2.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package assignments;
+
+/**
+ * 
+ */
+public class DailyAssignment9_Pattern2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=1;i<=5;i++) { 
+			for(int j=1;j<=i;j++) {
+				
+			System.out.print(j);
+			}
+			
+			System.out.println("");
+		}
+	}
+
+}
