8ac1d5666a51c3d5082f1407c63d64d3761dcdc0
vishnuvardhanp1 <vishnu.vis2007@gmail.com>
2026-04-04 16:01:18 +0530

    Apr 04 weekly assigment program 1 Smallest in arrays

--- src/assignments/WeeklyAssignment3_Array1.java
+++ src/assignments/WeeklyAssignment3_Array1.java
@@ -13,32 +13,37 @@ public class WeeklyAssignment3_Array1 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int arr[] = {1,1,1,1,1,1};
-		int smallest =arr[0];
-		int secondSmallest=999999;
+		int arr[] = {4,2,7,2,9,1};
+		//int arr[] = {1,1,1,1,1,1};
+		int smallest;
+		int secondSmallest;
+		//intialize smallest and second smallest
+		if(arr[0] < arr[1]) {
+			smallest=arr[0];
+			secondSmallest=arr[1];
+		} else {
+			smallest=arr[1];
+			secondSmallest=arr[0];
+		}
 		//Traverse and find smallest
-		for(int i=0;i<arr.length;i++) {
+		for(int i=2;i<arr.length;i++) {
 			
 			if(arr[i] < smallest) {
+				secondSmallest = smallest;
 				smallest =arr[i];
+			} else if (arr[i] > smallest && arr[i] < secondSmallest) {
+				secondSmallest=arr[i];	
 			}
 		}
-		//Traverse again to find second smallest
-		for(int i=0;i<arr.length;i++) {
-			
-			if(arr[i] > smallest && arr[i] < secondSmallest) {
-				
-					secondSmallest=arr[i];
-				
-			}
-		}
+	
 		System.out.println("The smallest element is "+ smallest);
+		
 		//Handle case when all elements are same
-		if(secondSmallest == 999999) {
+		if(secondSmallest == smallest) {
 		    System.out.println("No second smallest element");
 		} else {
 		System.out.println("The second smallest element is "+ secondSmallest);
-		}
+		} 
 	}
 
 }
