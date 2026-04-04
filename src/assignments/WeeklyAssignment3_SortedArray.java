9df785112f414c6ce1c534c7e8629f4a1add2c9f
vishnuvardhanp1 <vishnu.vis2007@gmail.com>
2026-04-04 16:23:52 +0530

    Apr 04 weekly assigment program 1 checking sorted arrays

---
+++ src/assignments/WeeklyAssignment3_SortedArray.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package assignments;
+
+/**
+ * 
+ */
+public class WeeklyAssignment3_SortedArray {
+
+	
+	static boolean isSorted(int arr[])
+	{
+		
+		 for(int i = 1; i < arr.length; i++) {
+		        if(arr[i] < arr[i - 1]) {
+		            return false;
+		        }
+		    }
+
+		    return true;
+	
+		
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int arr[] = {1,2,3,4};
+       // int arr[]= {1,3,2};
+      //  int arr[]= {-1,0,1};
+        boolean isAscending=isSorted(arr);
+        System.out.println(isAscending);
+        
+	}
+
+}
