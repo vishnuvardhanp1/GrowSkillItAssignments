73c15443e2f10499076580c29176300eb2336fe2
vishnuvardhanp1 <vishnu.vis2007@gmail.com>
2026-04-09 19:30:20 +0530

    Apr 09 Daily Assignment program 2 left rotate array

---
+++ src/assignments/DailyAssignment15_LeftRotateArray.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package assignments;
+
+/**
+ * 
+ */
+public class DailyAssignment15_LeftRotateArray {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int arr[]= {1,2,3,4};
+        int first=arr[0];
+        for(int i=0;i<arr.length-1;i++) {
+        	arr[i]=arr[i+1];
+        }
+        arr[arr.length-1]=first;
+        for(int num :arr) {
+        	System.out.print(num + " ");
+        }
+	}
+
+}
