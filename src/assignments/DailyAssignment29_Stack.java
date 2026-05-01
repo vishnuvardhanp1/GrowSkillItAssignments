b40e6d8bb7d213be115438e85b773b1cbe2c01c9
vishnuvardhanp1 <vishnu.vis2007@gmail.com>
2026-05-01 07:55:44 +0530

    Apr 30 stack dailyassignment

---
+++ src/assignments/DailyAssignment29_Stack.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package assignments;
+
+import java.util.Stack;
+
+/**
+ *  Reverse a String Using Stack
+
+Question:
+Take a string input from the user and reverse it using Stack<Character>.
+
+Example:
+
+Input:
+
+hello
+
+Output:
+
+olleh
+ */
+public class DailyAssignment29_Stack {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Stack<Character> stack=new Stack<Character>();
+   String str="hello";
+   for(int i=0;i<=str.length()-1;i++) {
+	   stack.push(str.charAt(i));
+	   
+   }
+   System.out.println("Input " + str);
+   System.out.print("Output ");
+   while(!stack.empty()){
+	   System.out.print(stack.pop());
+   }
+    
+	}
+
+}
